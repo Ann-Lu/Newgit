52d9a0e64a0ec77b34c0eb9adf0f1134f6e98e62
Ann <laqww6447@163.com>
2020-09-11 20:41:31 +0800

    html+css+js

---
+++ hello world/2/hw.js
@@ -0,0 +1,14 @@
+// 通过class更改属性
+function change() {
+    document.getElementById('hello').className = "red";
+}
+function recover() {
+    document.getElementById("hello").className = ' ';
+}
+// 通过样式更改属性
+/* function change() {
+    document.getElementById("hw").style.color = 'red';
+}
+function recover() {
+    document.getElementById("hw").style.color = '';
+} */
